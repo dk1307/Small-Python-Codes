b346e36b6c5cfbe2959371babf47073ce5996232
Darshak <kdarshak1307@gmail.com>
2021-05-19 11:43:49 +0530

    code dictionary

--- dictionary/app1.py
+++ dictionary/app1.py
@@ -1,20 +1,24 @@
 import json
-from difflib import get_close_matches
+from difflib import get_close_matches #import important lib
+#open different json files
 with open("dictionary.json") as fin1:
     data1 = json.load(fin1)
 with open("076 data.json") as fin2:
     data2 = json.load(fin2)
 with open("dictionary_compact.json") as fin3:
     data3 = json.load(fin3)
+#update also say merged all data to data1 file
 data1.update(data2)
 data1.update(data3)
+#merged file data1 to new merged dump
 with open("merged.json", "w") as fout:
     json.dump(data1, fout)
-
+#load merged.json file
 data = json.load(open("merged.json"))
-
+#crate custom search function
 def serch(w):
-        d=w.lower()
+        d=w.lower()#all words to lower
+        # if else conditons to check differnt conditions
         if d in data:
             return data[d]
         elif w.title() in data:
@@ -32,7 +36,7 @@ def serch(w):
         else:
             return ("HEyy word not found,please double check")
 print("\nWelcome to My Customize Dictionary \n")
-word=input("HEyy Enter the word you have to Search: ")
+word=input("HEyy Enter the word you have to Search: ")#user input
 op=serch(word)
 if type(op) == list:
     for i in op:
